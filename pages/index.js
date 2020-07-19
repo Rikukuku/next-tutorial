1e45fb00c1fe953be9b98fce9ad73cb6fa5e1009
Rikukuku <ricky_gland821_0714@yahoo.co.jp>
2020-07-19 19:29:13 +0900

    Initial commit

---
+++ pages/index.js
@@ -0,0 +1,57 @@
+import Head from "next/head";
+import Layout, { siteTitle } from "../components/layout";
+import utilsStyles from "../styles/utils.module.css";
+import { getSortedPostsData } from "../lib/posts";
+import Link from "next/link";
+import Date from "../components/date";
+
+export default function Home({ allPostsData }) {
+  return (
+    <Layout home>
+      <Head>
+        <title>{siteTitle}</title>
+      </Head>
+      <section className={utilsStyles.headingMd}>
+        <p>Nextが得意です</p>
+        <p>
+          (This is a sample website - you’ll be building a site like this on{" "}
+          <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
+        </p>
+      </section>
+      <section className={`${utilsStyles.headingMd} ${utilsStyles.padding1px}`}>
+        <h2 className={utilsStyles.headingLg}>Blog</h2>
+        <ul className={utilsStyles.list}>
+          {allPostsData.map(({ id, date, title }) => (
+            <li className={utilsStyles.listItem} key={id}>
+              <Link href="/posts/[id]" as={`/posts/${id}`}>
+                <a>{title}</a>
+              </Link>
+              <br />
+              <small className={utilsStyles.lightText}>
+                <Date dateString={date} />
+              </small>
+            </li>
+          ))}
+        </ul>
+      </section>
+    </Layout>
+  );
+}
+// work on ssg
+export async function getStaticProps() {
+  const allPostsData = getSortedPostsData();
+  return {
+    props: {
+      allPostsData,
+    },
+  };
+}
+// work on ssr
+// export async function getServerSideProps(context) {
+//   return{
+//     props: {}
+//   }
+// }
+// Build時にデータを取得する => SSG(ただし開発環境ではリクエスト時にgetStaticPropsが発動する)
+// リクエスト時にデータを取得する => SSR(getServerSidePropsを使用する)
+// データ不要の部分だけssgしてクライアントサイドでデータfetchすることも可能
